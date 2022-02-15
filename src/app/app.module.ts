d27591d6881830a04464d538d76b2726768e9bd9
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-02-15 16:50:39 -0300

    Cree y separe el proyecto en componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AboutmeComponent } from './componentes/aboutme/aboutme.component';
+import { ExperienceComponent } from './componentes/experience/experience.component';
+import { EducationComponent } from './componentes/education/education.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProjectsComponent } from './componentes/projects/projects.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutmeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
