55dd5f108586fef3a2648ae2980258ddc8416f8c
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-02-28 20:47:46 -0300

    AboutMe vinculado a JSON

--- src/app/componentes/aboutme/aboutme.component.ts
+++ src/app/componentes/aboutme/aboutme.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-aboutme',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutme.component.css']
 })
 export class AboutmeComponent implements OnInit {
+  miPortfolio:any;
 
-  constructor() { }
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.miPortfolio=data;
+    })
   }
 
 }
