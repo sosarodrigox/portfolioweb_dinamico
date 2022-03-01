f459416607a5cc059f8c08a7f8b6b0ab391506df
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-02-28 23:25:10 -0300

    Experience vinculado a JSON

--- src/app/componentes/experience/experience.component.ts
+++ src/app/componentes/experience/experience.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-experience',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent implements OnInit {
-
-  constructor() { }
+  experienciaList:any;
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+  this.datosPortfolio.obtenerDatos().subscribe(data =>{
+    console.log(data);
+    this.experienciaList=data.experiencia;
+  });
   }
 
 }
