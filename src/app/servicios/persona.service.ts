855ad4ff3cdb8680a4572f42905098ff4d27e5dd
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-05-16 23:10:42 -0300

    min 16 video

---
+++ src/app/servicios/persona.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { persona } from '../model/persona.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonaService {
+
+  //Primero ponemos desde donde quiero que llame al backend
+  URL = 'http://localhost:8080/';
+
+  constructor(private http:HttpClient) { }
+
+  //El observable lo utilizo para las peticiones asíncronas
+  public getPersona(): Observable<persona>{
+    return this.http.get<persona>(this.URL+)
+  }
+}
