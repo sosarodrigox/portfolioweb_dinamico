a75233352464fce8c052455e6dadd0638d999a99
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-06-12 20:21:52 -0300

    Funciona crear curso

--- src/app/componentes/crear-curso/crear-curso.component.ts
+++ src/app/componentes/crear-curso/crear-curso.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { edu_curso } from 'src/app/model/edu_curso.model';
+import { CursoService } from 'src/app/servicios/curso.service';
 
 @Component({
   selector: 'app-crear-curso',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearCursoComponent implements OnInit {
 
-  constructor() { }
+  curso : edu_curso = new edu_curso("", "", "","","","")
+
+  constructor(private cursoService:CursoService, private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  crearCurso(){
+    this.cursoService.crearCurso(this.curso).subscribe(dato =>{
+      console.log(dato);
+      this.volverPortfolio();
+    });
+  }
+
+  volverPortfolio(){
+    this.router.navigate(['/portfolio']);
+  }
+
+  onSubmit(){
+    this.crearCurso();
   }
 
 }
