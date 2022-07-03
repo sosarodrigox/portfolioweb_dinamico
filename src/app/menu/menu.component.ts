776b2cc66619a65ed4b1bc0e75a2eb539225db10
Rodrigo Sosa <sosarodrigox@gmail.com>
2022-07-03 11:59:18 -0300

    cap 10 - funciona login logout

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { persona } from '../model/persona.model';
 import { PersonaService } from '../servicios/persona.service';
+import { TokenService } from '../servicios/token.service';
 
 @Component({
   selector: 'app-menu',
@@ -10,12 +11,24 @@ import { PersonaService } from '../servicios/persona.service';
 })
 export class MenuComponent implements OnInit {
 
+  isLogged = false;
   persona: persona = new persona("","","","","","","","","","","","","","","","","","","","");
 
-  constructor(public persoService: PersonaService, private router:Router) { }
+  constructor(public persoService: PersonaService, private tokenService: TokenService, private router:Router) { }
 
   ngOnInit(): void {
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    }else{
+      this.isLogged = false;
+    }
     this.persoService.getPersona().subscribe(data =>{this.persona = data})
   }
+
+  onLogOut(): void{
+    this.tokenService.logOut();
+    window.location.reload();
+  }
+  
 
 }
